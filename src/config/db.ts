579e2b7011fc6a8b81b2ec6de7b32a42b6199761
Yisus550 <jesus.alfonso2402@gmail.com>
2025-06-29 11:29:25 -0700

    refactor: update Sequelize model path resolution to use path.join for better compatibility

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,11 +1,18 @@
 import { Sequelize } from "sequelize-typescript";
 import dotenv from "dotenv";
+import path from "path";
 
 dotenv.config();
 
 const db = new Sequelize(process.env.DATABASE_URL!, {
-    models: [__dirname + "/../models/**/*"],
-    logging: false,
+  models: [path.join(__dirname, "/../models/**/*")],
+  logging: false,
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    },
+  },
 });
 
 export default db;
